28161b3d7d2d72a64c74e5417f1bee30bbd87414
Francis Breault <1347720@cstj.qc.ca>
2021-08-31 19:55:59 -0400

    Finalisation

--- Project - Exercice 2/Vertical.cpp
+++ Project - Exercice 2/Vertical.cpp
@@ -1,19 +1,43 @@
-//But:Écrire un programme qui lit un entier et qui détermine et affiche s’il s’agit d’un entier pair d’un entier impair.
+//But:Écrire un programme qui prend comme entrée un nombre de cinq chiffres. Le programme vérifie qu'il y a bien
+//5 chiffres dans le nombre.Puis il affiche le nombre verticalement, le premier chiffre sur la première ligne, le
+//deuxième chiffre sur la deuxième ligne, ainsi de suite.
 //Auteur:Francis Breault
 //Date:2021-08-31
 
+
+
+// liste des bibliothèques utilisés
 #include<iostream>
 
+// Pour éviter l'usage des std::
 using namespace std;
-void main()
+
+void main() // Utilisation de void pour éviter de retourner une valeur a la fin
 {
 	setlocale(LC_ALL, "");
 
 	//Déclaration des variables
+	int nombreEntier;
+	
+
+	//Demande à l'utilisateur de saisir un nombre
+	cout << "Veuillez saisr un nombre a 5 chiffre : ";
+	cin >> nombreEntier;
 
 
+	if (nombreEntier >= 10000 && nombreEntier <= 99999 || nombreEntier >= -99999 && nombreEntier <= -10000)
+	{
 
-
+		cout << nombreEntier / 10000 << endl;
+		cout << nombreEntier / 1000 % 10  << endl;
+		cout << nombreEntier / 100 % 10 << endl;
+		cout << nombreEntier / 10 % 10 << endl;
+		cout << nombreEntier % 10 << endl;
+	}
+	else
+	{
+		cout << "Ce nombre ne contient pas 5 chiffres";
+	}
 
 
 
--- Projet - Exercice 1/PairImpair.cpp
+++ Projet - Exercice 1/PairImpair.cpp
@@ -1,20 +1,45 @@
-//But:Écrire un programme qui prend comme entrée un nombre de cinq chiffres. Le programme vérifie qu'il y a bien
-//5 chiffres dans le nombre.Puis il affiche le nombre verticalement, le premier chiffre sur la première ligne, le
-//deuxième chiffre sur la deuxième ligne, ainsi de suite.P
+//But:Écrire un programme qui lit un entier et qui détermine et affiche s’il s’agit d’un entier pair d’un entier impair.
 //Auteur:Francis Breault
 //Date:2021-08-31
 
+
+// liste des bibliothèques utilisés
 #include<iostream>
 
+
+// Pour éviter l'usage des std::
 using namespace std;
-void main()
+
+void main() // Utilisation de void pour éviter de retourner une valeur a la fin
 {
 	setlocale(LC_ALL, "");
 
 	//Déclaration des variables
+	int nombre;
+	
+	cout << "Veuillez entrée un nombre entier : " << endl;
+	cin >> nombre;
+	cout << endl;
 
+	if (nombre % 2 == 0 ) //un nombre pair est divisible par 2 et on verifie avec le modulo qu'il reste aucun entier apres la division
+	{
+		cout << nombre << " est un nombre pair" << endl;
 
+	}
+	else
+	{
+		cout << nombre << " est un nombre impair" << endl;
 
+	}
+
+	/*
+	plan de test
+	nombre1				résultat escompté
+	85					Est un nombre impair
+	3650				Est un nombre pair
+	-5841				Est un nombre impair
+	12548				Est un nombre pair
+	*/
 
 
 
