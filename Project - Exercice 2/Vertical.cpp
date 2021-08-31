4c059e74c01e9fcd2dc4f710b0aac9c80e1a216a
FrancisBreault <FrancisBreault@DESKTOP-LFB4FNU>
2021-08-31 18:40:29 -0400

    Ajoutez des fichiers projet.

---
+++ Project - Exercice 2/Vertical.cpp
@@ -0,0 +1,22 @@
+//But:Écrire un programme qui lit un entier et qui détermine et affiche s’il s’agit d’un entier pair d’un entier impair.
+//Auteur:Francis Breault
+//Date:2021-08-31
+
+#include<iostream>
+
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	//Déclaration des variables
+
+
+
+
+
+
+
+
+
+}
---
+++ Projet - Exercice 1/PairImpair.cpp
@@ -0,0 +1,24 @@
+//But:Écrire un programme qui prend comme entrée un nombre de cinq chiffres. Le programme vérifie qu'il y a bien
+//5 chiffres dans le nombre.Puis il affiche le nombre verticalement, le premier chiffre sur la première ligne, le
+//deuxième chiffre sur la deuxième ligne, ainsi de suite.P
+//Auteur:Francis Breault
+//Date:2021-08-31
+
+#include<iostream>
+
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	//Déclaration des variables
+
+
+
+
+
+
+
+
+
+}
